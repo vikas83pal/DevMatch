8c23511ba1b76c371f91d3078abce392af48a169
vikas83pal <vikas83pal@gmail.com>
2025-04-24 11:57:10 +0530

    release/v1

---
+++ src/main/java/com/example/DevMatch/DevMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevMatchApplication.class, args);
+	}
+
+}
