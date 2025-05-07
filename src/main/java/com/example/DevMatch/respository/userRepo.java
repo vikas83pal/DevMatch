415ee12c5bf387ec7f8ef54c2f04ae6ba2960e90
vikas83pal <vikas83pal@gmail.com>
2025-05-07 17:13:42 +0530

    release

--- src/main/java/com/example/DevMatch/respository/userRepo.java
+++ src/main/java/com/example/DevMatch/respository/userRepo.java
@@ -3,9 +3,10 @@ package com.example.DevMatch.respository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.DevMatch.model.User; 
+import com.example.DevMatch.model.User;
 
+ 
 @Repository
-public interface userRepo  extends JpaRepository<User, Integer>{
-	
+public interface userRepo extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
